9211555a1917389fd436a1b69f2d8e49c6633482
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-17 04:13:46 -0300

    mobile nav with icon

--- src/components/Navbar/nav/MobileNav/MobileNav.tsx
+++ src/components/Navbar/nav/MobileNav/MobileNav.tsx
@@ -2,20 +2,22 @@ import allProjects from "@/Projects/allProjects";
 import { NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
 import Link from "next/link";
 import ProjectsLinkNav from "../WindowNav/ProjectsLinkNav";
+import { FaBriefcase, FaHome } from "react-icons/fa";
+
 interface Props {}
 const MobileNav: React.FC<Props> = () => {
   return (
     <>
       {/* Menu desplegable en mobile  */}
       <NavbarMenu className="bg-neutral-900/5">
-        <NavbarMenuItem>
-          <Link className=" text-xl" href={"/"}>
-            Inicio
-          </Link>
+        <NavbarMenuItem className="flex items-center gap-2 my-2  text-2xl">
+          <FaHome /> <Link href={"/"}>Inicio</Link>
         </NavbarMenuItem>
-        <NavbarMenuItem className="text-xl">Proyectos</NavbarMenuItem>
+        <NavbarMenuItem className="flex items-center gap-2 my-2 text-2xl">
+          <FaBriefcase /> Proyectos
+        </NavbarMenuItem>
         {allProjects.map((item, index) => (
-          <NavbarMenuItem className="ml-3 relative" key={`${item}-${index}`}>
+          <NavbarMenuItem className="ml-10 relative" key={`${item}-${index}`}>
             <ProjectsLinkNav p={item} />
           </NavbarMenuItem>
         ))}
