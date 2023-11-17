3502fc7dc7c557d6d6fee7c4280c3ea1698c1ab1
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-17 02:25:26 -0300

    Projects class is ok

--- src/Projects/class/Projects.ts
+++ src/Projects/class/Projects.ts
@@ -47,18 +47,7 @@ class Project {
     this.tech = tech;
   }
 
-  public returnColor = (): string => {
-    const colorVariants: Record<typesMainColor, string> = {
-      cyan: "text-cyan-500 before:bg-cyan-500 ",
-      red: "text-red-600 before:bg-red-500 ",
-      yellow: "text-yellow-600 before:bg-yellow-500 ",
-      green: "text-yellow-600 before:bg-yellow-500 ",
-      orange: "text-yellow-600 before:bg-yellow-500 ",
-      purple: "text-yellow-600 before:bg-yellow-500 ",
-      violet: "text-yellow-600 before:bg-yellow-500 ",
-    };
-    return colorVariants[this.colorMain] || "";
-  };
+  
 }
 
 export default Project;
