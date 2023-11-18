8e539a7db5ee72083969e68e25863b70160254ab
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-18 00:37:38 -0300

    projects add data

--- src/Projects/data/cuota-admin.ts
+++ src/Projects/data/cuota-admin.ts
@@ -1,12 +1,30 @@
 import Project, { ProjectTypes } from "../class/Projects";
 
 const data: ProjectTypes = {
-  name: "cuota-admin",
+  name: "Cuota-Admin",
   extendedDescription:
-    "Esta va a ser la descripcion general de todo el proyecto si o si requerida",
+    "Cuota-Admin es un sistema de control de cuotas y resumen de mes para un gimnasio familiar, el proyecto se encuentra en producción desde principios de 2023 y para 2024 se vienen cambios muy grandes en la app, se van a agregar funciones que interactúan con los clientes, entre otras cosas.",
   description: "Sistema de control de cuotas para un gym",
   colorMain: "red",
   state: "En Producción",
+  urlGithub: "https://github.com/gonzaqepasa/cuota-admin",
+  tech: [
+    "firebase",
+    "nextjs",
+    "typescript",
+    "tailwind",
+    "react",
+    "postgres",
+    "node",
+  ],
+  images: [
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F1.jpeg?alt=media&token=b0e044bf-e354-478e-a552-f36db1b2f841",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F2.jpeg?alt=media&token=3841065b-bebc-4c67-88ca-4455b2137a3b",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F3.jpeg?alt=media&token=ec7fbe4c-47d8-472b-98f6-d2f951e022dd",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F4.jpeg?alt=media&token=8d2d3723-8562-4902-99ee-18f21cedfb7f",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F5.jpeg?alt=media&token=8062e4cb-7e4c-46c9-86a4-679961a6f3db",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fcuotaadmin%2F6.jpeg?alt=media&token=62eff898-5e5a-4d31-9327-f2ba5d5d1c34",
+  ],
 };
 
 const cuota_admin = new Project(data);
--- src/Projects/data/share-it.ts
+++ src/Projects/data/share-it.ts
@@ -3,10 +3,28 @@ import Project, { ProjectTypes } from "../class/Projects";
 const data: ProjectTypes = {
   name: "Share It",
   extendedDescription:
-    "Esta va a ser la descripcion general de todo el proyecto si o si requerida",
+    "ShareIt es una red social con funcionalidades como seguir usuarios, hacer posteos con fotos, dar me gusta, hacer comentarios, etc. También tiene un sistema de pago para adquirir una membresía premium, entre otras funciones.",
   description: "Red social realizada en Henry para rendir final",
   colorMain: "yellow",
   state: "Versión Beta",
+  urlGithub:"https://github.com/henry19ag15/finalproject",
+  tech: [
+    "express",
+    "firebase",
+    "javascript",
+    "node",
+    "sass",
+    "redux",
+    "react",
+    "postgres",
+  ],
+  images: [
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fshareit%2F4.jfif?alt=media&token=607aaaa1-1ac3-46f4-ac7f-f375c29b4060",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fshareit%2F1.jfif?alt=media&token=dc6d3ad6-f579-49fe-b1b4-c4117b1b11d2",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fshareit%2F3.jfif?alt=media&token=aa8829cd-2d7f-476f-b175-4471b2027bf3",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fshareit%2F2.jfif?alt=media&token=59040fb6-0278-4f36-824b-3da7b6038ead",
+    "https://firebasestorage.googleapis.com/v0/b/sodapop-9f1d7.appspot.com/o/portafolio%2Fshareit%2F5.jfif?alt=media&token=ce1c727f-9250-41d5-b374-9ad2e9d21e39",
+  ],
 };
 
 const share_it = new Project(data);
