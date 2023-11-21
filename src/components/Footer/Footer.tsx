7a01b30493e35fd7585e381efd9bcfd6deb2eb4c
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-21 04:55:10 -0300

    Footer h

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,22 @@
 import SocialMediaButton from "../globals/SocialMediaButton";
 import Wave from "../globals/Wave/Wave";
+import { MdEmail, MdPhone } from "react-icons/md";
 
 const FooterMain = () => {
   return (
     <>
       <Wave />
-      <footer className="flex min-h-[20vh] flex-col text-neutral-900  pb-6  items-center relative bg-white">
-        <div className=" flex py-2  flex-col text-sm">
-          <p className="text-base">Contacto:</p>
+      <footer className="flex min-h-[50vh] flex-col text-neutral-900  pb-6  items-center justify-evenly relative bg-white">
+        <div className=" flex py-2  flex-col text-base">
+          <p className="">Contacto:</p>
 
           <a
             href="mailto:gonzalovam@hotmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-neutral-500 hover:text-blue-600 transition-colors"
+            className="flex items-center gap-2  text-neutral-500 hover:text-blue-600 transition-colors"
           >
+            <MdEmail />
             gonzalovam@hotmail.com
           </a>
 
@@ -22,12 +24,13 @@ const FooterMain = () => {
             href="https://wa.me/+541153741713"
             target="_blank"
             rel="noopener noreferrer"
-            className=" text-neutral-500 hover:text-green-600 transition-colors"
+            className=" flex items-center gap-2  text-neutral-500 hover:text-green-600 transition-colors"
           >
+            <MdPhone />
             +54 11-53741713
           </a>
         </div>
-        <div className=" w-11/12 flex">
+        <div className=" lg:w-11/12 my-5 flex justify-end">
           <SocialMediaButton background />
         </div>
         <div className="absolute bottom-0">
