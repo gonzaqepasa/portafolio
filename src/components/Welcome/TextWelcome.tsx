32284435aa1456a1a975d7b3c1f9a1d1101c7d46
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-11-22 14:39:49 -0300

    new text

--- src/components/Welcome/TextWelcome.tsx
+++ src/components/Welcome/TextWelcome.tsx
@@ -1,12 +1,13 @@
 function TextWelcome() {
   const currentYear = new Date().getFullYear();
-  const birthYear = 1995;
-  const age = currentYear - birthYear;
+  const startProgramming = 2020;
+  const age = currentYear - startProgramming;
   return (
     <div className={`px-5 flex flex-col text-center text-neutral-400 `}>
-      <i>{`Hola, mi nombre es Gonzalo Martinez, tengo ${age} años y naci en Argentina`}</i>
-      <i>{`Estoy capacitado para trabajar con las diferentes tecnologias javascript que se encuentran en npmjs.`}</i>
-      <i>{`Algunas de las que utilizo actualmente son:`}</i>
+      <i>{`Hola, mi nombre es Gonzalo Martinez, hace ${age} años estudio y practico tecnologias Javascript, tanto para el front-end como para el back-end.`}</i>
+      <i>{`Me desenvuelvo agilmente haciendo uso de varios modulos que se encuentran en npmjs.`}</i>
+      {/* <i>{`Estoy capacitado para trabajar con diferentes tecnologias javascript que se encuentran en npmjs.`}</i> */}
+      {/* <i>{`Algunas de las que utilizo actualmente son:`}</i> */}
     </div>
   );
 }
